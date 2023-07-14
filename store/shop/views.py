51382969b5df98a1e8dc466e627be8be38f36ed8
khalid <khalidcomilla58@gmail.com>
2023-07-15 03:39:01 +0600

    Fixed the issue . Happened because of spelling of template(instead of templates). Closed the server and re-ran it.

--- store/shop/views.py
+++ store/shop/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,"shop/index.html")
+     #return HttpResponse('hello')
+     return render(request,'index.html')
--- store/store/settings.py
+++ store/store/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
