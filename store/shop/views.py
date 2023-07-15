ff229a7360f47fa40994b90716d70ad8ce974999
khalid <khalidcomilla58@gmail.com>
2023-07-15 22:31:59 +0600

    static file tested

--- store/shop/views.py
+++ store/shop/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
      #return HttpResponse('hello')
-     return render(request,'index.html')
+     return render(request,'shop/index.html')
