8eba9c85f27eb4870b4d1f086afe44b546d010e0
khalid <khalidcomilla58@gmail.com>
2023-07-16 20:29:42 +0600

    more tables added by models.py

--- store/shop/models.py
+++ store/shop/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length = 50)
+    category = models.CharField(max_length = 50 , default = "")
+    subCategory = models.CharField(max_length = 50 , default = "")
+    price = models.IntegerField(default = 0)
     desc = models.CharField(max_length = 300)
     pub_date = models.DateField()
-    
+    image = models.ImageField(upload_to = "shop/images", default = "")
