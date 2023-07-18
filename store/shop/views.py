19a4496807afd19682bd647455b8fb63dec4005f
khalid <khalidcomilla58@gmail.com>
2023-07-18 21:46:52 +0600

    homepage updated

--- store/shop/views.py
+++ store/shop/views.py
@@ -6,14 +6,21 @@ from math import ceil
 from django.http import HttpResponse
 
 def index(request):
-    products= Product.objects.all()
-    n= len(products)
-    nSlides= n//4 + ceil((n/4) + (n//4))
-    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
-    return render(request,"shop/index.html", params)
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+        
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    return render(request,'shop/about.html')
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse("We are at contact")
